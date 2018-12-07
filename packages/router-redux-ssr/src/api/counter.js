a46812224878b40d0638072f2818b6b8aa6412c0
leejh3224 <leejh3224@naver.com>
2018-12-07 19:48:47 +0900

    add redirection logic in serverRender function

--- packages/router-redux-ssr/src/api/counter.js
+++ packages/router-redux-ssr/src/api/counter.js
@@ -3,5 +3,5 @@ const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
 export const fetchCounter = callback => {
     setTimeout(() => {
         callback(getRandomInt(1, 100))
-    }, 1000)
+    }, 300)
 }
--- packages/router-redux-ssr/src/serverRender.jsx
+++ packages/router-redux-ssr/src/serverRender.jsx
@@ -12,42 +12,50 @@ import { fetchCounter } from './api/counter'
 
 export default (req, res) => {
     fetchCounter(result => {
-        const title = 'hello, router-redux-ssr!'
-
-        res.write(
-            `<html><head><title>${title}</title></head><body style="margin: 0;"><div id="root">`
-        )
-
         const context = {}
-        const store = configureStore()
-        const sheet = new ServerStyleSheet()
-        const jsx = sheet.collectStyles(
-            <StaticRouter location={req.url} context={context}>
-                <Provider store={store}>
-                    <Routes />
-                </Provider>
-            </StaticRouter>
-        )
-        const stream = sheet.interleaveWithNodeStream(renderToNodeStream(jsx))
 
-        stream.pipe(
-            res,
-            { end: false }
-        )
+        if (context.url) {
+            res.writeHead(301, { location: context.url })
+            res.end()
+        } else {
+            const title = 'hello, router-redux-ssr!'
 
-        const preloadedState = { counter: result } || store.getState()
+            res.write(
+                `<html><head><title>${title}</title></head><body style="margin: 0;"><div id="root">`
+            )
 
-        stream.on('end', () =>
-            res.end(`
-                    </div>
-                    <script>
-                        window.__PRELOADED_STATE__ = ${JSON.stringify(
+            const store = configureStore()
+            const sheet = new ServerStyleSheet()
+            const jsx = sheet.collectStyles(
+                <StaticRouter location={req.url} context={context}>
+                    <Provider store={store}>
+                        <Routes />
+                    </Provider>
+                </StaticRouter>
+            )
+            const stream = sheet.interleaveWithNodeStream(
+                renderToNodeStream(jsx)
+            )
+
+            stream.pipe(
+                res,
+                { end: false }
+            )
+
+            const preloadedState = { counter: result } || store.getState()
+
+            stream.on('end', () =>
+                res.end(`
+                        </div>
+                        <script>
+                            window.__PRELOADED_STATE__ = ${JSON.stringify(
         preloadedState
     ).replace(/</g, '\\u003c')}
-                    </script>
-                    <script src="dist/bundle.js"></script>
-                </body>
-            </html>`)
-        )
+                        </script>
+                        <script src="dist/bundle.js"></script>
+                    </body>
+                </html>`)
+            )
+        }
     })
 }
